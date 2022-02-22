0bc2cc183af963825ecc42383204896c66f6ac5e
edimulyadi <51184976+edimulyadi@users.noreply.github.com>
2022-02-22 16:01:38 +0700

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,3 +1,7 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://klinikpintar.id/api/";
-export default axios;
+export default axios.create({
+  baseURL: "https://api.staging.klinikpintar.id/hc/open/health-center?page=1&limit=5",
+  headers: {
+    "X-Medigo-Client-Key": "994e23e67b6cb551a2951f8abaf2aa5265d5c2fb63a5679457a596efb8444270"
+  }
+});
